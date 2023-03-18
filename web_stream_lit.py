743bf23d1f7d5e3ef676e8996c9d2b6dd74ade42
LeNick86 <90423559+LeNick86@users.noreply.github.com>
2023-03-18 16:41:04 -0300

    Update web_stream_lit.py

--- web_stream_lit.py
+++ web_stream_lit.py
@@ -157,17 +157,22 @@ def main():
         removed_data,filtered_df = post_process(filtered_df,removed_data)
         st.subheader("Algunas palabras que no entraron: ")
         removed_data = removed_data.reset_index()
+        del removed_data['index']
         st.dataframe(removed_data.head(150).style.set_properties(**{'text-align': 'left'}).format({'Size': '{:,.0f}'}),width=300)
 
         st.subheader("Primeras 250 no @ que no entraron: ")
         removed_data2 = removed_data[~removed_data['Text'].str.startswith('@')]
         removed_data2 = removed_data2.reset_index()
+        del removed_data2['index']
+        del removed_data2['level_0']
         st.dataframe(removed_data2.head(250).style.format({'Size': '{:,.0f}'}),width=300)
     
         
         st.subheader("Primeros 70 @ que no entraron: ")
         removed_data3 = removed_data[removed_data['Text'].str.startswith('@')]
         removed_data3 = removed_data3.reset_index()
+        del removed_data3['index']
+        del removed_data3['level_0']
         st.dataframe(removed_data3.head(70).style.set_properties(**{'text-align': 'left'}).format({'Size': '{:,.0f}'}),width=300)
     
         st.title("Poner en el word art: ")
